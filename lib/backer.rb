bc1084583574e6f319ad14e6c5474a6886c0b6aa
An Pham <anpham148@gmail.com>
2020-07-10 12:08:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,7 @@ class Backer
   end
 
   def back_project(project)
-    if project.backers.include?(self)
+    if !project.backers.include?(self)
      @backed_projects << project
      project.add_backer(self)
     end
